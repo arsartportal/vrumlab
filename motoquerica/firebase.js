03d533352feb3477f97121547a9ef9de0f216ec3
Ars <arlissonferreira@gmail.com>
2026-04-20 17:16:17 -0300

    consertando alguns bugs

--- motoquerica/firebase.js
+++ motoquerica/firebase.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-analytics.js";
 
@@ -12,4 +13,20 @@ const firebaseConfig = {
 };
 
 export const app = initializeApp(firebaseConfig);
+=======
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-analytics.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDunks_5ddHx5Dtd9ldanQR72KyKJO8Y1k",
+  authDomain: "motoquerica.firebaseapp.com",
+  projectId: "motoquerica",
+  storageBucket: "motoquerica.firebasestorage.app",
+  messagingSenderId: "431543378491",
+  appId: "1:431543378491:web:d6a6b33d273f562a25321e",
+  measurementId: "G-KZXJ2DZN6C"
+};
+
+export const app = initializeApp(firebaseConfig);
+>>>>>>> 8ac68c1 (consertando alguns bugs)
 export const analytics = getAnalytics(app);
